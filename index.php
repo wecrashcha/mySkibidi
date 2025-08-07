ca8ed56a3ac5772c01e643f77cef00ad6a36a802
wecrashchaaaaaa <mdzakirafifh96999@gmail.com>
2025-08-07 14:14:06 +0700

    File Original

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php
+class mySkibidi {
+    public $skibidi;
+    public $gyatt;
+
+    public function SkibidiGyatt() {
+        return "myskibidi";
+    }
+}
+
+$skibidi = new mySkibidi();
+
+$skibidi->SkibidiGyatt();
+?>
